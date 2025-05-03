002804d021b602f9adaaadc70453102770ddef70
Watcher <sudiptoghosh2003@gmail.com>
2025-05-03 21:24:27 +0600

    solve

---
+++ 520A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,count=0;
+    cin>>n;
+    string s;
+    cin>>s;
+
+    for(int i=0; i<n; i++){
+        s[i] = tolower(s[i]);
+    }
+    sort(s.begin(),s.end());//sort function for char (begin to end).
+
+    for(int i=0; i<n; i++){
+        if(s[i] != s[i+1]){
+            count++;
+        }
+    }
+    if(count==26){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+
+    
+}

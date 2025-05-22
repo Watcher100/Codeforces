5499fbfddb348862612eee88d2dd83aea902a86f
Watcher <sudiptoghosh2003@gmail.com>
2025-05-22 14:32:12 +0600

    solve

---
+++ 32B.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    string s;
+    cin>>s;
+
+    for(int i=0; i<s.size(); i++){
+        if(s[i]=='.'){
+            cout<<"0";
+        }else if(s[i]=='-' && s[i+1]=='.'){
+            cout<<"1";
+            i++;
+        }else if(s[i]=='-' && s[i+1]=='-'){
+            cout<<"2";
+            i++;
+        }
+    }
+    // string c;
+    // for(int i=0; i<s.size(); i++){
+    //     if(s[i]=='.'){
+    //         c+='0';
+    //     }else if(s[i]=='-' && s[i+1]=='.'){
+    //         c+='1';
+    //         i++;
+    //     }else if(s[i]=='-' && s[i+1]=='-'){
+    //         c+='2';
+    //         i++;
+    //     }
+    // }
+    // cout<<c<<endl;
+
+}

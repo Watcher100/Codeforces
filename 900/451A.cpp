57a0b956e3bccd3f322a8f15e56e164d639a9b65
Watcher <sudiptoghosh2003@gmail.com>
2025-07-11 23:52:47 +0600

    solve

---
+++ 900/451A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int n,m;
+    cin>>n>>m;
+    int a=min(n,m);
+
+    if(a%2==0){
+        cout<<"Malvika"<<endl;
+    }else{
+        cout<<"Akshat"<<endl;
+    }
+    
+
+}
